865ad7ff3d97ac68ab074c1035655ddb54ea83e0
Mnw2212 <mayuresh2212@gmail.com>
2014-12-11 22:21:19 +0530

    FIleIndexing using Lucene.
    The required jars have been added to lib folder.

--- src/Hash.java
+++ src/Hash.java
@@ -1,86 +1,78 @@
 import java.sql.*;
 import java.io.File;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.IOException;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
 import org.apache.commons.io.FilenameUtils;
 
 public class Hash{
 	
-	static final String JDBC_DRIVER="com.mysql.jdbc.Driver";
-	static final String DB_URL="jdbc:mysql://localhost:3306/FileHash";
-	static final String USER="root";
-	static final String PASS="<Password>";
+	
 	// NEW STUFF =======
 	static List<String> filename = new ArrayList<String>();
     static List<String> dirname = new ArrayList<String>();
     static List<String> filepath = new ArrayList<String>();
     static List<String> dirpath = new ArrayList<String>();
+    // LUCENE DECLARATIONS
+    static Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_4_9);
     // END OF NEW DECLRATIONS
+    
 	
-	public static void main(String args[])throws ClassNotFoundException, SQLException, NoSuchAlgorithmException{
-		String maindir="/home/mayuresh/work/";
+	public static void main(String args[])throws ClassNotFoundException, SQLException, NoSuchAlgorithmException, IOException, ParseException{
+		String maindir="/home/mayuresh/Desktop/random";
 		
 		Hash obj = new Hash();
 		obj.walk(maindir);
-		
+		File wareHouse = new File("/home/mayuresh/Desktop/");
+	    Directory directory = FSDirectory.open(wareHouse);
 		
 		try{
-			Connection conn=null;
-			Class.forName("com.mysql.jdbc.Driver");
 			
-			System.out.print("Connecting to database...\n");
-			conn = DriverManager.getConnection(DB_URL,USER,PASS);
-			System.out.print("Connected\n");
-			
-			String sql1 = "insert into FileHash(md5,filename,filepath) values(?,?,?)";
-			String sql2 = "insert into DirHash(md5,dirname,dirpath) values(?,?,?)";
-			//===============================================================
-			
-			PreparedStatement stmt = conn.prepareStatement(sql1);
-			
-			System.out.println("Database connection made. Hashing Files...");
-			String Hashval;
-			String name;
-			
+			String name;			
 			for(int i=0;i<filename.size();i++){
+				
 				name = FilenameUtils.removeExtension((filename.get(i)));
-				//System.out.print(name);
-				Hashval=md5(name);
-				stmt.setString(1,Hashval);
-				stmt.setString(2,name);
-				stmt.setString(3,maindir);
-				stmt.addBatch();
+				Document doc = new Document();
+				System.out.println("Adding fields to doc");
+				doc.add(new Field("filename",name, TextField.TYPE_STORED));
+				doc.add(new Field("filepath", maindir, TextField.TYPE_STORED));
+				Indexer(doc);
 			}
-			stmt.executeBatch();
 			
-			System.out.println("File hash done. Batch executed");
-			System.out.println("Starting directory hash...");
-			
-			stmt = conn.prepareStatement(sql2);
 			for(int i=0;i<dirname.size();i++){
 				name = dirname.get(i);
-				//System.out.print(name);
-				Hashval=md5(name);
-				stmt.setString(1,Hashval);
-				stmt.setString(2,name);
-				stmt.setString(3,dirpath.get(i));
-				stmt.addBatch();
+				Document doc = new Document();
+				doc.add(new Field("dirname",name, TextField.TYPE_STORED));
+				doc.add(new Field("dirpath", dirpath.get(i), TextField.TYPE_STORED));
+				Indexer(doc);
 			}
-			stmt.executeBatch();
+			
 			System.out.println("Directory hash done. Batch executed");
-			conn.close();		
 			System.out.println("Connection closed. Good stuff.");
-			}catch(SQLException e){
-				System.out.println(e);			
-			}catch(ClassNotFoundException ce){
-				System.out.println(ce);
+			Search("hey",directory);
+			}finally{
+				System.out.println("Process Completed");
 			}
 	}
 	
-	
-	public void walk( String path ) {
+	private void walk( String path ) {
         
         File root = new File( path );
         File[] list = root.listFiles();
@@ -99,17 +91,31 @@ public class Hash{
             }
         }
     }
+	
+	private static void Indexer(Document Doc) throws IOException{
+		File wareHouse = new File("/home/mayuresh/Desktop/");
+	    Directory directory = FSDirectory.open(wareHouse);
+		IndexWriterConfig  config = new IndexWriterConfig(Version.LUCENE_4_9, analyzer);
+		IndexWriter iwriter = new IndexWriter(directory,config);
+		iwriter.addDocument(Doc);
+		System.out.println("Document added : "+Doc);
+		iwriter.close();
 		
-	public static String md5(String x) throws NoSuchAlgorithmException{
-		try{
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			
-			md.update(x.getBytes());
-			String hash = md.digest().toString();
-			return hash;
-		}catch(NoSuchAlgorithmException e1){
-			String hash="err1";
-			return hash;
-		}
+	}
+	private static void Search(String text,Directory directory) throws IOException, ParseException{
+		DirectoryReader ireader = DirectoryReader.open(directory);
+		IndexSearcher isearcher = new IndexSearcher(ireader);
+		QueryParser parser = new QueryParser(Version.LUCENE_4_9, "dirpath",analyzer);
+	    Query query = parser.parse(text);
+	    ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
+	    System.out.println("===============================\n");
+	    System.out.println(hits);
+	    for (int i = 0; i < hits.length; i++) {
+		      Document hitDoc = isearcher.doc(hits[i].doc);
+		      System.out.println(hitDoc);
+		    }
+	    System.out.println("Completed");
+	    ireader.close();
+	    directory.close();
 	}
 }	
